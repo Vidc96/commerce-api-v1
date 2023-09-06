572ac780ea4660cc94ce9c84c09cb4b74b0b2f3b
Victor Chinea <victord.chineap@gmail.com>
2023-09-06 20:20:05 +0200

    [NEW] build product model

--- models/product.js
+++ models/product.js
@@ -1,11 +1,55 @@
 const mongoose = require('mongoose')
 
 const productShema = mongoose.Schema({
-  name: String,
-  image: String,
+  name: {
+    type: String,
+    require: true
+  },
+  description: {
+    type: String,
+    require: true
+  },
+  richDescription: {
+    type: String,
+    default: ''
+  },
+  image: {
+    type: String,
+    default: ''
+  },
+  Images: {
+    type: String,
+  },
+  brand: {
+    type: String,
+    default: ''
+  },
+  price: {
+    type: Number,
+    default: 0
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true
+  },
   countInStock: {
     type: Number,
-    required: true
+    required: true,
+    min: 0,
+    max: 255
+  },
+  rating: {
+    type: Number,
+    default: 0
+  },
+  isFeatured: {
+    type: Boolean,
+    default: false
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now
   }
 })
 
