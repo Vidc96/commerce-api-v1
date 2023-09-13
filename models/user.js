7fb43daa2feaebd25834bc83a89fa3b4788be65b
Victor Chinea <victord.chineap@gmail.com>
2023-09-13 16:51:27 +0200

    [ADD] model user created

--- models/user.js
+++ models/user.js
@@ -1,12 +1,55 @@
 const mongoose = require('mongoose')
 
 const userShema = mongoose.Schema({
-  name: String,
-  image: String,
-  countInStock: {
-    type: Number,
-    required: true
-  }
+  name: {
+    type: String,
+    require: true
+  },
+  email: {
+    type: String,
+    require: true
+  },
+  passwordHash: {
+    type: String,
+    require: true
+  },
+  phone: {
+    type: String,
+    require: true
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  },
+  street: {
+    type: String,
+    default: ''
+  },
+  apartment: {
+    type: String,
+    default: ''
+  },
+  zip: {
+    type: String,
+    default: ''
+  },
+  city: {
+    type: String,
+    default: '',
+  },
+  country: {
+    type: String,
+    default: ''
+  },
+  
+})
+
+userShema.virtual('id').get(function () {
+  return this._id.toHexString()
+})
+
+userShema.set('toJSON', {
+  virtuals: true,
 })
 
 exports.User = mongoose.model('User', userShema)
