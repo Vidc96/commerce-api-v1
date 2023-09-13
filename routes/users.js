a7c616af5bf03968aa6d7d296b2d5dcc700411d3
Victor Chinea <victord.chineap@gmail.com>
2023-09-13 19:28:57 +0200

    [ADD] hashing password user

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,14 @@
-const { User } = require('../models/user');
-const express = require('express');
+const { User } = require('../models/user')
+const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
 
 router.post(`/`, async (req, res) => {
+  const saltRounds = 10
   let user = new User({
     name: req.body.name,
     email: req.body.email,
-    passwordHash: req.body.passwordHash,
+    passwordHash: bcrypt.hashSync(req.body.passwordHash, saltRounds),
     phone: req.body.phone,
     isAdmin: req.body.isAdmin,
     street: req.body.stree,
