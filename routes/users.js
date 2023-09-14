809fb5635b2ea3aa039258a79a11122a872fe7a5
Victor Chinea <victord.chineap@gmail.com>
2023-09-14 20:09:41 +0200

    [ADD] post/login method using hashing and token

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const { User } = require('../models/user')
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 router.post(`/`, async (req, res) => {
   const saltRounds = 10
@@ -11,7 +12,7 @@ router.post(`/`, async (req, res) => {
     passwordHash: bcrypt.hashSync(req.body.passwordHash, saltRounds),
     phone: req.body.phone,
     isAdmin: req.body.isAdmin,
-    street: req.body.stree,
+    street: req.body.street,
     apartment: req.body.apartment,
     zip: req.body.zip,
     city: req.body.city,
@@ -24,6 +25,29 @@ router.post(`/`, async (req, res) => {
   return res.status(400).send('The user cannot be creted!')
 
   res.send(user)
+})
+
+router.post(`/login`, async (req, res) => {
+  const user = await User.findOne({email: req.body.email})
+  const secret = process.env.SECRET
+
+  if(!user) {
+    return res.status(400).send(`User don't found!`)
+  }
+
+  if(user && bcrypt.compareSync(req.body.passwordHash, user.passwordHash)) {
+    const token = jwt.sign(
+      {
+        userId: user.id
+      },
+      secret,
+      {expiresIn: '1d'}
+    )
+    res.status(200).send({user: user.email, token: token})
+  } else {
+    res.status(400).send(`Password is wrong!.`)
+  }
+
 })
 
 module.exports = router
