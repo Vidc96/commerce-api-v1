c25ddcb79d89b9eec5d0875b574433875614a929
Victor Chinea <victord.chineap@gmail.com>
2023-10-05 17:23:41 +0200

    [NEW] authentication error handling

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,11 +1,19 @@
-const { expressjwt: jwt } = require("express-jwt");
+const { expressjwt: jwt } = require("express-jwt")
 
 function authJwt() {
-  const secret = process.env.secret;
+  const secret = process.env.SECRET
+  const api = process.env.API_URL
   return jwt({
     secret,
     algorithms: ["HS256"],
-  });
+  }).unless({
+    path: [
+      {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
+      {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
+      '/api/v1/users/login/',
+      '/api/v1/users/register/'
+    ]
+  })
 }
 
-module.exports = authJwt;
+module.exports = authJwt
