9672f9176048c96f8479e0f9883eac6ddbbc3ddc
Victor Chinea <victord.chineap@gmail.com>
2023-10-05 19:50:14 +0200

    [ADD] user endpoints count and delete

--- routes/users.js
+++ routes/users.js
@@ -123,4 +123,27 @@ router.post('/register', async (req,res)=>{
   res.send(user);
 })
 
+router.delete('/:id', (req, res)=>{
+  User.findByIdAndRemove(req.params.id).then(user =>{
+      if(user) {
+          return res.status(200).json({success: true, message: 'the user is deleted!'})
+      } else {
+          return res.status(404).json({success: false , message: "user not found!"})
+      }
+  }).catch(err=>{
+     return res.status(500).json({success: false, error: err}) 
+  })
+})
+
+router.get(`/get/count`, async (req, res) =>{
+  const userCount = await User.countDocuments((count) => count)
+
+  if(!userCount) {
+      res.status(500).json({success: false})
+  } 
+  res.send({
+      userCount: userCount
+  });
+})
+
 module.exports = router
