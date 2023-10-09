76b280bd228cdf64bb6c603e09f4fbb71366e258
Victor Chinea <victord.chineap@gmail.com>
2023-10-09 14:01:03 +0200

    [NEW] orders and orders-items model scheme

--- models/order.js
+++ models/order.js
@@ -1,12 +1,7 @@
 const mongoose = require('mongoose')
 
 const orderShema = mongoose.Schema({
-  name: String,
-  image: String,
-  countInStock: {
-    type: Number,
-    required: true
-  }
+  
 })
 
 exports.Order = mongoose.model('Order', orderShema)
